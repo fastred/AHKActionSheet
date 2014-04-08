ade6ffb83ddf4e43e3025fcbb963a5f02cb9fb98
Arkadiusz Holko <fastred@fastred.org>
2014-04-08 13:06:26 +0200

    Add Snapshots category on UIView

---
+++ Classes/UIView+Snapshots.h
@@ -0,0 +1,15 @@
+//
+//  UIView+Snapshots.h
+//  AHKActionSheetExample
+//
+//  Created by Arkadiusz on 08-04-14.
+//  Copyright (c) 2014 Arkadiusz Holko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Snapshots)
+
+- (UIImage *)snapshotImage;
+
+@end
