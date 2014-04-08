d2e31763622fb06797a63e81b0b75172987e11f6
Arkadiusz Holko <fastred@fastred.org>
2014-04-08 11:28:04 +0200

    Add a view controller with button in the example project

--- AHKActionSheetExample/AHKActionSheetExample/AHKViewController.m
+++ AHKActionSheetExample/AHKActionSheetExample/AHKViewController.m
@@ -14,16 +14,19 @@
 
 @implementation AHKViewController
 
+#pragma mark - UIViewController
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
+#pragma mark - Actions
+
+- (IBAction)showActionSheetTapped:(id)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
 }
 
 @end
