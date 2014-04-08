6d95deff4befe3c2429571e8a91b3d04314c0198
Arkadiusz Holko <fastred@fastred.org>
2014-04-08 11:48:00 +0200

    Add AHKActionSheet.h/.m files

---
+++ Classes/AHKActionSheet.h
@@ -0,0 +1,13 @@
+//
+//  AHKActionSheet.h
+//  AHKActionSheetExample
+//
+//  Created by Arkadiusz on 08-04-14.
+//  Copyright (c) 2014 Arkadiusz Holko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AHKActionSheet : UIView
+
+@end
---
+++ Classes/AHKActionSheet.m
@@ -0,0 +1,13 @@
+//
+//  AHKActionSheet.m
+//  AHKActionSheetExample
+//
+//  Created by Arkadiusz on 08-04-14.
+//  Copyright (c) 2014 Arkadiusz Holko. All rights reserved.
+//
+
+#import "AHKActionSheet.h"
+
+@implementation AHKActionSheet
+
+@end
