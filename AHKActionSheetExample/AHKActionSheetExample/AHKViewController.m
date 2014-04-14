35667f481663cf79ef010247da282dd422dd6082
Arkadiusz Holko <fastred@fastred.org>
2014-04-14 16:14:03 +0200

    Add two examples (default and customized) to the example project

--- AHKActionSheetExample/AHKActionSheetExample/AHKViewController.m
+++ AHKActionSheetExample/AHKActionSheetExample/AHKViewController.m
@@ -26,16 +26,101 @@
 
 #pragma mark - Actions
 
-- (IBAction)showActionSheetTapped:(id)sender
+- (IBAction)showDefaultTapped:(id)sender
 {
-    AHKActionSheet *actionSheet = [[AHKActionSheet alloc] initWithTitle:NSLocalizedString(@"Test test", nil)];
-    [actionSheet addButtonWithTitle:NSLocalizedString(@"Button 1", nil)
+    AHKActionSheet *actionSheet = [[AHKActionSheet alloc] initWithTitle:NSLocalizedString(@"Lorem ipsum dolor sit amet, consectetur adipiscing elit?", nil)];
+
+    [actionSheet addButtonWithTitle:NSLocalizedString(@"Info", nil)
+                              image:[UIImage imageNamed:@"Icon1"]
                                type:AHKActionSheetButtonTypeDefault
                             handler:^(AHKActionSheet *actionSheet) {
-                                NSLog(@"Button 1 tapped");
+                                NSLog(@"Info tapped");
+                            }];
+
+    [actionSheet addButtonWithTitle:NSLocalizedString(@"Add to Favorites", nil)
+                              image:[UIImage imageNamed:@"Icon2"]
+                               type:AHKActionSheetButtonTypeDefault
+                            handler:^(AHKActionSheet *actionSheet) {
+                                NSLog(@"Favorite tapped");
+                            }];
+
+    [actionSheet addButtonWithTitle:NSLocalizedString(@"Share", nil)
+                              image:[UIImage imageNamed:@"Icon3"]
+                               type:AHKActionSheetButtonTypeDefault
+                            handler:^(AHKActionSheet *actionSheet) {
+                                NSLog(@"Share tapped");
+                            }];
+
+    [actionSheet addButtonWithTitle:NSLocalizedString(@"Delete", nil)
+                              image:[UIImage imageNamed:@"Icon4"]
+                               type:AHKActionSheetButtonTypeDestructive
+                            handler:^(AHKActionSheet *actionSheet) {
+                                NSLog(@"Delete tapped");
                             }];
 
     [actionSheet show];
+}
+
+- (IBAction)showCustomizedTapped:(id)sender
+{
+    AHKActionSheet *actionSheet = [[AHKActionSheet alloc] initWithTitle:nil];
+
+    actionSheet.blurTintColor = [UIColor colorWithWhite:0.0f alpha:0.75f];
+    actionSheet.blurRadius = 8.0f;
+    actionSheet.buttonHeight = 50.0f;
+    actionSheet.separatorColor = [UIColor colorWithWhite:1.0f alpha:0.3f];
+    actionSheet.selectedBackgroundColor = [UIColor colorWithWhite:0.0f alpha:0.5f];
+    UIFont *defaultFont = [UIFont fontWithName:@"Avenir" size:17.0f];
+    actionSheet.buttonTextAttributes = @{ NSFontAttributeName : defaultFont,
+                                          NSForegroundColorAttributeName : [UIColor whiteColor] };
+    actionSheet.destructiveButtonTextAttributes = @{ NSFontAttributeName : defaultFont,
+                                          NSForegroundColorAttributeName : [UIColor redColor] };
+    actionSheet.cancelButtonTextAttributes = @{ NSFontAttributeName : defaultFont,
+                                          NSForegroundColorAttributeName : [UIColor whiteColor] };
+
+    UIView *headerView = [[self class] fancyHeaderView];
+    actionSheet.headerView = headerView;
+
+    [actionSheet addButtonWithTitle:NSLocalizedString(@"Info", nil)
+                              image:[UIImage imageNamed:@"Icon1"]
+                               type:AHKActionSheetButtonTypeDefault
+                            handler:nil];
+
+    [actionSheet addButtonWithTitle:NSLocalizedString(@"Add to Favorites", nil)
+                              image:[UIImage imageNamed:@"Icon2"]
+                               type:AHKActionSheetButtonTypeDefault
+                            handler:nil];
+
+    for (int i = 0; i < 5; i++) {
+        [actionSheet addButtonWithTitle:[NSString stringWithFormat:@"Share %d", i]
+                                  image:[UIImage imageNamed:@"Icon3"]
+                                   type:AHKActionSheetButtonTypeDefault
+                                handler:nil];
+    }
+
+    [actionSheet addButtonWithTitle:NSLocalizedString(@"Delete", nil)
+                              image:[UIImage imageNamed:@"Icon4"]
+                               type:AHKActionSheetButtonTypeDestructive
+                            handler:nil];
+
+    [actionSheet show];
+}
+
+#pragma mark - Private
+
++ (UIView *)fancyHeaderView
+{
+    UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 200, 60)];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Cover"]];
+    imageView.frame = CGRectMake(10, 10, 40, 40);
+    [headerView addSubview:imageView];
+    UILabel *label1 = [[UILabel alloc] initWithFrame:CGRectMake(60, 20, 200, 20)];
+    label1.text = @"Some helpful description";
+    label1.textColor = [UIColor whiteColor];
+    label1.font = [UIFont fontWithName:@"Avenir" size:17.0f];
+    [headerView addSubview:label1];
+
+    return  headerView;
 }
 
 @end
