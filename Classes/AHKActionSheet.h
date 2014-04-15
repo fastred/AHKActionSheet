959d5a1d8544e3dd3d31361bc43f6c0e8c9ae381
Arkadiusz Holko <fastred@fastred.org>
2014-04-15 10:27:18 +0200

    Add better comments in the header file

--- Classes/AHKActionSheet.h
+++ Classes/AHKActionSheet.h
@@ -21,15 +21,22 @@ typedef void(^AHKActionSheetHandler)(AHKActionSheet *actionSheet);
 
 // Appearance
 
+/**
+ *  See UIImage+AHKAdditions.h/.m to learn how these three properties are used.
+ */
 @property (nonatomic) CGFloat blurRadius UI_APPEARANCE_SELECTOR;
 @property (strong, nonatomic) UIColor *blurTintColor UI_APPEARANCE_SELECTOR;
 @property (nonatomic) CGFloat blurSaturationDeltaFactor UI_APPEARANCE_SELECTOR;
-/// Height of the button (internally it's a UITableViewCell)
+
+/// Height of the button (internally it's a UITableViewCell).
 @property (nonatomic) CGFloat buttonHeight UI_APPEARANCE_SELECTOR;
+/// Height of the cancel button (internally it's a UIButton).
 @property (nonatomic) CGFloat cancelButtonHeight UI_APPEARANCE_SELECTOR;
-/// If set, a small shadow (a gradient layer) will be drawn above the cancel button to separate it visually from the other buttons.
-/// It's best to use a color similar (but maybe with a lower alpha value) to blurTintColor.
-/// See "Advanced" example in the example project to see it used.
+/**
+ *  If set, a small shadow (a gradient layer) will be drawn above the cancel button to separate it visually from the other buttons.
+ * It's best to use a color similar (but maybe with a lower alpha value) to blurTintColor.
+ * See "Advanced" example in the example project to see it used.
+ */
 @property (strong, nonatomic) UIColor *cancelButtonShadowColor UI_APPEARANCE_SELECTOR;
 /// Boxed (@YES, @NO) boolean value (enabled by default)
 @property (strong, nonatomic) NSNumber *automaticallyTintButtonImages UI_APPEARANCE_SELECTOR;
@@ -39,6 +46,7 @@ typedef void(^AHKActionSheetHandler)(AHKActionSheet *actionSheet);
 @property (strong, nonatomic) UIColor *separatorColor UI_APPEARANCE_SELECTOR;
 /// Background color of the button when it's tapped (internally it's a UITableViewCell)
 @property (strong, nonatomic) UIColor *selectedBackgroundColor UI_APPEARANCE_SELECTOR;
+/// Text attributes of the title (passed in the initializer or set via `title` property)
 @property (copy, nonatomic) NSDictionary *titleTextAttributes UI_APPEARANCE_SELECTOR;
 @property (copy, nonatomic) NSDictionary *buttonTextAttributes UI_APPEARANCE_SELECTOR;
 @property (copy, nonatomic) NSDictionary *destructiveButtonTextAttributes UI_APPEARANCE_SELECTOR;
@@ -49,11 +57,12 @@ typedef void(^AHKActionSheetHandler)(AHKActionSheet *actionSheet);
 @property (copy, nonatomic) NSString *cancelButtonTitle;
 /// Action sheet title shown above the buttons.
 @property (copy, nonatomic) NSString *title;
-/// View shown above the first button (only if the title isn't set).
+/// View shown above the buttons (only if the title isn't set).
 @property (strong, nonatomic) UIView *headerView;
 /// Window visible before the actionSheet was presented.
 @property (weak, nonatomic, readonly) UIWindow *previousKeyWindow;
 
+// Designated initializer.
 - (instancetype)initWithTitle:(NSString *)title;
 - (void)addButtonWithTitle:(NSString *)title image:(UIImage *)image type:(AHKActionSheetButtonType)type handler:(AHKActionSheetHandler)handler;
 - (void)addButtonWithTitle:(NSString *)title type:(AHKActionSheetButtonType)type handler:(AHKActionSheetHandler)handler;
