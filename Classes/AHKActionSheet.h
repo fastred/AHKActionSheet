3becc34d63fe40469f76bff3b2a15fd3c8c30f04
Arkadiusz Holko <fastred@fastred.org>
2014-05-20 21:01:25 +0200

    Move animationDuration property closer to other appearance related properties

--- Classes/AHKActionSheet.h
+++ Classes/AHKActionSheet.h
@@ -52,6 +52,8 @@ typedef void(^AHKActionSheetHandler)(AHKActionSheet *actionSheet);
 @property (copy, nonatomic) NSDictionary *buttonTextAttributes UI_APPEARANCE_SELECTOR;
 @property (copy, nonatomic) NSDictionary *destructiveButtonTextAttributes UI_APPEARANCE_SELECTOR;
 @property (copy, nonatomic) NSDictionary *cancelButtonTextAttributes UI_APPEARANCE_SELECTOR;
+/// Duration of the show/dismiss animations. Defaults to 0.5.
+@property (nonatomic) NSTimeInterval animationDuration UI_APPEARANCE_SELECTOR;
 
 
 /// A handler called on every type of dismissal (tapping on "Cancel" or swipe down or flick down).
@@ -63,8 +65,6 @@ typedef void(^AHKActionSheetHandler)(AHKActionSheet *actionSheet);
 @property (strong, nonatomic) UIView *headerView;
 /// Window visible before the actionSheet was presented.
 @property (weak, nonatomic, readonly) UIWindow *previousKeyWindow;
-/// Duration of the show/dismiss animations. Defaults to 0.5.
-@property (nonatomic) NSTimeInterval animationDuration UI_APPEARANCE_SELECTOR;
 
 
 /**
