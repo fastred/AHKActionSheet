7dbc5af5cbb7dcf26d5edcfa0c3cc4c0b909e074
Arkadiusz Holko <fastred@fastred.org>
2014-05-22 15:29:40 +0200

    Add unit tests
    For the simplicity they’re located in the AHKActionSheetExample project

--- AHKActionSheetExample/AHKActionSheetExampleTests/AHKActionSheetExampleTests.m
+++ AHKActionSheetExample/AHKActionSheetExampleTests/AHKActionSheetExampleTests.m
@@ -7,9 +7,24 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "AHKActionSheet.h"
+
+@implementation UIView (Tests)
+/// Check recursively if the view or one its subviews contains the view
+- (BOOL)containsView:(UIView *)otherView
+{
+    for (UIView *subview in self.subviews) {
+        if (subview == otherView || [subview containsView:otherView]) {
+            return YES;
+        }
+    }
+
+    return NO;
+}
+@end
 
 @interface AHKActionSheetExampleTests : XCTestCase
-
+@property (strong, nonatomic) AHKActionSheet *actionSheet;
 @end
 
 @implementation AHKActionSheetExampleTests
@@ -18,6 +33,9 @@
 {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    self.actionSheet = [[AHKActionSheet alloc] init];
+    [self.actionSheet addButtonWithTitle:@"Test" type:AHKActionSheetButtonTypeDefault handler:nil];
 }
 
 - (void)tearDown
@@ -26,9 +44,37 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testNoButtons
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    AHKActionSheet *actionSheetWithoutButtons = [[AHKActionSheet alloc] init];
+    XCTAssertThrows([actionSheetWithoutButtons show], @"Should fail an assertion.");
+}
+
+- (void)testWithButtons
+{
+    XCTAssertNoThrow([self.actionSheet show], @"Shouldn't fail an assertion.");
+}
+
+- (void)testPresentedInNewWindow
+{
+    XCTAssertFalse([[UIApplication sharedApplication].keyWindow containsView:self.actionSheet], @"Key window shouldn't contain the action sheet");
+
+    [self.actionSheet show];
+
+    XCTAssertTrue([[UIApplication sharedApplication].keyWindow containsView:self.actionSheet], @"Key window should contain the action sheet");
+}
+
+- (void)testCancelHandler
+{
+    __block BOOL cancelled = NO;
+    self.actionSheet.cancelHandler = ^(AHKActionSheet *actionSheet){
+        cancelled = YES;
+    };
+
+    [self.actionSheet show];
+    [self.actionSheet dismissAnimated:NO];
+
+    XCTAssertTrue(cancelled, @"`cancelled` should be true");
 }
 
 @end
